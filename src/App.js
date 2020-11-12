97973159d0cb47ba0e6a6e11b4f1d39b93946d6d
PratikBingewar <bingewarpratik@gmail.com>
2020-11-12 11:07:31 +0530

    React condtionals in JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { render, Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  func = ()  => {
+    return 'pratik'
+  };
+  render() {
+    const name = 'Patrick Bingewar'
+    const loading = 'true'
+    return (  
+      <div className="App">
+        {loading ? <h4>loading...</h4> : <h1>Hello { name.toUpperCase() } and { this.func() }</h1>}
+      </div>
+    );
+
+  }
+
+  
 }
 
 export default App;
